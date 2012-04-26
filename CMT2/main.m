0b0adf6d98b55abc4d3dbf7ab733a2719cabdc96
remillet <remillet@mac.com>
2012-04-26 00:22:41 -0700

    Initial Commit

---
+++ CMT2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CMT2
+//
+//  Created by Sara Duckler on 4/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
